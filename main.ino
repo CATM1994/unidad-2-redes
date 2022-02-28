71a71ecc15c64bf0105881bd8bf9cf02e1174b38
CATM1194 <carlos.torres@colima.tecnm.mx>
2022-02-28 08:05:35 -0600

    primer problema

--- main.ino
+++ main.ino
@@ -1,4 +1,5 @@
 byte byteRead;
+char direccion1 [10] ="0ShAn1";
 
 void setup(){
     Serial.begin(9600);
@@ -7,7 +8,9 @@ void setup(){
 void loop(){
    
         
-        Serial.write("hola mundo ");
-        delay(5000);
+        Serial.write("comando obtenido:\n");
+        delay(1000);
+        Serial.write(direccion1);
+       // Serial.write("0ShaAn1");
     
 }
